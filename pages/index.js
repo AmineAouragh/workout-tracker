dbafc0fb001bb604106994047dabf55b658ab326
Amine Aouragh <amine.aouragh.20@gmail.com>
2024-02-08 13:50:40 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import Image from "next/image";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { supabase } from './api/supabase'
+import Head from 'next/head'
 
 export default function Home() {
 
   const [ workoutActivity, setWorkoutActivity ] = useState('')
+  const [ workoutDay, setWorkoutDay ] = useState('')
   const [ reps, setReps ] = useState(0)
   const [ sets, setSets ] = useState(0)
+  const [ username, setUsername ] = useState('')
 
   async function addNewWorkoutEntry(){
     const { data, error } = await supabase
-    .from('users')
+    .from('activities')
     .insert([
-      { name: workoutActivity, reps: reps, sets: sets }
+      { name: workoutActivity, day: workoutDay, reps: reps, sets: sets }
     ])
   }
 
@@ -62,38 +65,54 @@ export default function Home() {
     }
 
     let year = new Date().getFullYear()
-
+    setWorkoutDay(`${month} ${day}, ${year}`)
     return `${month} ${day}, ${year}`
   }
+
+  useEffect(() => {
+    setDate()
+  }, [])
   
   return (
-    <div className="px-4 py-8 flex flex-col min-h-screen w-full justify-center items-center">
-      <h1 className="text-4xl xl:text-8xl text-transparent bg-clip-text bg-gradient-to-r from-gray-600 to-gray-900 font-bold">Workout Tracker</h1>
-      <h2 className="mt-6 text-2xl text-gray-500 font-medium">Fitness Tracking For Workout Enthusiasts 💪</h2>
-      <form className="mt-16 border-2 border-gray-800 flex flex-col justify-center rounded-md px-8 py-8 w-full sm:w-2/3 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
+    <>
+      <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=Rubik+Doodle+Shadow&display=swap" rel="stylesheet" />
+            <title>Workout Tracker</title>
+      </Head>
+      <div className="px-4 py-8 flex flex-col min-h-screen w-full justify-center items-center">
+      <h1 className="text-4xl xl:text-8xl text-transparent font-poppins bg-clip-text bg-gradient-to-r from-gray-600 to-gray-900 font-bold">Workout Tracker</h1>
+      <h2 className="mt-6 text-2xl text-gray-500 font-poppins font-medium">Fitness Tracking For Workout Enthusiasts 💪</h2>
+      <form className="mt-10 border-2 border-gray-800 flex flex-col justify-center rounded-md px-8 py-8 w-full sm:w-2/3 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
         <h3 className="text-4xl font-bold mb-3">Log your workout now</h3>
-        <h4 className="text-gray-500 font-medium mb-5">{setDate()}</h4>
+        <h4 className="text-gray-500 font-medium mb-5">{workoutDay}</h4>
         <div className="flex flex-col mb-4">
           <label htmlFor="activity" className="text-lg font-medium mb-2">Workout activity:</label>
           <input id="activity" type="text" placeholder="Push-ups, Sit-ups..." className="hidden border-2 outline-none border-gray-500 focus:border-gray-800 rounded-md px-3 py-2 text-xl font-bold" required />
-          <select className="rounded-md border-2 border-gray-500 focus:border-gray-800 px-3 py-2 text-xl font-bold">
+          <select value={workoutActivity} onChange={e => setWorkoutActivity(e.target.value)} className="font-poppins rounded-md border-2 border-gray-500 focus:border-gray-800 px-2 py-2 text-xl font-bold">
             <option value="push-ups" className="bg-gray-700 text-gray-50">---</option>
             <option value="push-ups" className="bg-gray-700 text-gray-50">Push-ups</option>
             <option value="sit-ups" className="bg-gray-700 text-gray-50">Sit-ups</option>
             <option value="squats" className="bg-gray-700 text-gray-50">Squats</option>
             <option value="bicep-curls" className="bg-gray-700 text-gray-50">Bicep curls</option>
+            <option value="running" className="bg-gray-700 text-gray-50">Running</option>
           </select>
         </div>
         <div className="flex flex-col mb-4">
             <label htmlFor="reps" className="text-lg font-medium mb-2">Reps:</label>
-            <input id="reps" type="number" min={1} className="border-2 border-gray-500 focus:border-gray-800 rounded-md outline-none px-3 py-2 text-xl font-bold" required />
+            <input id="reps" value={reps} onChange={e => setReps(e.target.value)} type="number" min={1} className="font-poppins border-2 border-gray-500 focus:border-gray-800 rounded-md outline-none px-3 py-2 text-xl font-bold" required />
+        </div>
+        <div className="flex flex-col mb-4">
+            <label htmlFor="sets" className="text-lg font-medium mb-2">Sets:</label>
+            <input id="sets" value={sets} onChange={e => setSets(e.target.value)} type="number" min={1} className="font-poppins border-2 border-gray-500 focus:border-gray-800 rounded-md outline-none px-3 py-2 text-xl font-bold" required />
         </div>
         <div className="flex flex-col">
-            <label htmlFor="sets" className="text-lg font-medium mb-2">Sets:</label>
-            <input id="sets" type="number" min={1} className="border-2 border-gray-500 focus:border-gray-800 rounded-md outline-none px-3 py-2 text-xl font-bold" required />
+          <label htmlFor="username" className="text-lg font-medium mb-2">Username:</label>
+          <input id="username" value={username} onChange={e => setUsername(e.target.value)} type="text" className="font-poppins border-2 border-gray-500 focus:border-gray-800 rounded-md outline-none px-3 py-2 text-xl font-bold" required />
         </div>
-        <button type="button" className="focus:ring ring-offset-2 focus:ring-gray-600 bg-gradient-to-r from-gray-600 to-gray-800 hover:from-gray-800 hover:to-gray-600 transition mt-10 text-xl text-gray-50 rounded-md px-5 py-3 font-bold">Add</button>
+        <button type="button" onClick={addNewWorkoutEntry} className="focus:ring ring-offset-2 focus:ring-gray-600 bg-gradient-to-r from-gray-600 to-gray-800 hover:from-gray-800 hover:to-gray-600 transition mt-10 text-xl text-gray-50 rounded-md px-5 py-3 font-bold">Add</button>
       </form>
-    </div>
+      <p className="bottom-2 absolute font-medium text-gray-700">Made by <span className="text-blue-500">@TheAmineAouragh</span></p>
+      </div>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        rubik: ["Rubik Doodle Shadow", 'serif'],
+        poppins: ["Poppins", "serif"]
+      }
     },
   },
   plugins: [],
